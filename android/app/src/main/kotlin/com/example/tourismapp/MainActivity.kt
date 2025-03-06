1ffc6b801f3e72af48681e9df504e4dcb802c5de
Mobile App Development <M.Develop@utas.edu.om>
2025-03-06 12:44:57 +0400

    initalized repo

---
+++ android/app/src/main/kotlin/com/example/tourismapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tourismapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHome());
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Tourism App",
+          style: TextStyle(
+            color: Colors.black,
+            letterSpacing: 3.0,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
